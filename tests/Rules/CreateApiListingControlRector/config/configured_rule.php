40f1d284e60fbf89a4b4a18d8630c06b219b2723
Michal Lulco <lulco@efabrica.sk>
2022-06-09 08:48:43 +0200

    Changed configs to use RectorConfig instead of ContainerConfigurator

--- tests/Rules/CreateApiListingControlRector/config/configured_rule.php
+++ tests/Rules/CreateApiListingControlRector/config/configured_rule.php
@@ -1,9 +1,8 @@
 <?php
 
+use Rector\Config\RectorConfig;
 use Rector\TomajNetteApi\Rules\CreateApiListingControlRector;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    $services->set(CreateApiListingControlRector::class);
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->rule(CreateApiListingControlRector::class);
 };
